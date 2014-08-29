f2e1a0fb4f5dd892b71ffe95e6a19b1e1ff16c39
Brook Elgie <specialbrew@gmail.com>
2014-08-29 10:54:57 +0100

    Simple add CartItem objects to Cart

--- store/cart.py
+++ store/cart.py
@@ -3,12 +3,29 @@ from decimal import Decimal
 
 class Cart(object):
 
+    def __init__(self):
+        self.items = []
+
+    def __len__(self):
+        return len(self.items)
+
+    def __getitem__(self, key):
+        return self.items[key]
+
     def get_total(self):
         '''Return sum of cart items as a Decimal.'''
         return Decimal(0)
 
+    def add(self, item):
+        '''Add an item to the cart.'''
+        cart_item = CartItem(item)
+        self.items.append(cart_item)
+
 
 class CartItem(object):
+
+    def __init__(self, product):
+        self.product = product
 
     def get_line_total(self):
         '''Return total as a Decimal.'''
--- store/tests.py
+++ store/tests.py
@@ -7,14 +7,33 @@ from cart import Cart, CartItem
 class CartTest(unittest.TestCase):
 
     def test_get_total_is_decimal(self):
-        '''Cart.get_total returns a Decimal.'''
+        '''Cart.get_total() returns a Decimal.'''
         cart = Cart()
         self.assertTrue(type(cart.get_total()) is Decimal)
+
+    def test_add_single_item(self):
+        '''Cart.add() a single item is successful.'''
+        cart = Cart()
+        cart.add('apple')
+        self.assertEqual(len(cart), 1)
+
+    def test_add_item_is_cartitem(self):
+        '''Cart contains a single CartItem after cart.add().'''
+        cart = Cart()
+        cart.add('apple')
+        self.assertTrue(type(cart[0]) is CartItem)
+
+    def test_add_two_items(self):
+        '''Adding more than one item increases cart length.'''
+        cart = Cart()
+        cart.add('apple')
+        cart.add('orange')
+        self.assertTrue(len(cart), 2)
 
 
 class CartItemTest(unittest.TestCase):
 
     def test_get_line_total_is_decimal(self):
-        '''Cart.get_line_total returns a Decimal.'''
-        cartitem = CartItem()
+        '''Cart.get_line_total() returns a Decimal.'''
+        cartitem = CartItem('apple')
         self.assertTrue(type(cartitem.get_line_total() is Decimal))
