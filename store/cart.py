2e5537cf1976d1fb96949fef19ac102bea2ca394
Brook Elgie <specialbrew@gmail.com>
2014-08-29 10:34:55 +0100

    Initial CartItem

--- store/cart.py
+++ store/cart.py
@@ -6,3 +6,10 @@ class Cart(object):
     def get_total(self):
         '''Return sum of cart items as a Decimal.'''
         return Decimal(0)
+
+
+class CartItem(object):
+
+    def get_line_total(self):
+        '''Return total as a Decimal.'''
+        return Decimal(0)
--- store/tests.py
+++ store/tests.py
@@ -1,7 +1,7 @@
 import unittest
 from decimal import Decimal
 
-from cart import Cart
+from cart import Cart, CartItem
 
 
 class CartTest(unittest.TestCase):
@@ -10,3 +10,11 @@ class CartTest(unittest.TestCase):
         '''Cart.get_total returns a Decimal.'''
         cart = Cart()
         self.assertTrue(type(cart.get_total()) is Decimal)
+
+
+class CartItemTest(unittest.TestCase):
+
+    def test_get_line_total_is_decimal(self):
+        '''Cart.get_line_total returns a Decimal.'''
+        cartitem = CartItem()
+        self.assertTrue(type(cartitem.get_line_total() is Decimal))
