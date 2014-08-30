28718a70d0e56f39e1f62797ea9947fc688bd25d
Brook Elgie <specialbrew@gmail.com>
2014-08-30 08:42:13 +0100

    Generalize multi buy classes into one MultiBuyOffer

--- store/offers.py
+++ store/offers.py
@@ -6,7 +6,7 @@ class AbstractOffer(object):
         self.target_product = target_product
 
     def calculate_line_total(self, cart_item, store):
-        '''All subclasses must impliment this method, returning a new total
+        '''All subclasses must implement this method, returning a new total
         for the cart_item.'''
         raise NotImplementedError()
 
@@ -19,23 +19,37 @@ class NoOffer(AbstractOffer):
         return cart_item.get_line_total(store)
 
 
-class BogofOffer(AbstractOffer):
-    '''A 'Buy One Get One Free' offer.'''
+class MultiBuyOffer(AbstractOffer):
+    '''
+    Buy a certain quantity to get another quantity free.
 
-    def calculate_line_total(self, cart_item, store):
-        '''Only charge for the approriate quantity of an item.'''
-        quotient, remainder = divmod(cart_item.quantity, 2)
-        charge_quantity = quotient + remainder
-        return store.get_product_price(cart_item.product) * charge_quantity
+    eg. a Buy One Get One Free offer would look like:
+        bogof_offer = MultiBuyOffer(1, 1, 'strawberries')
+    and a buy two get one free would look like:
+        multibuy_offer = MultiBuyOffer(2, 1, 'strawberries')
+    '''
 
-
-class BuyTwoGetThirdFreeOffer(AbstractOffer):
-    '''Buy two to get a third item free.'''
+    def __init__(self, target_product, charge_for_quantity, free_quantity, *args, **kwargs):
+        self.charge_for_quantity = charge_for_quantity
+        self.free_quantity = free_quantity
+        super(MultiBuyOffer, self).__init__(target_product, *args, **kwargs)
 
     def calculate_line_total(self, cart_item, store):
         '''Charge for multiples of the quotient and add remainder.'''
-        charge_for_quantity = 2
-        free_quantity = 1
-        quotient, remainder = divmod(cart_item.quantity, charge_for_quantity + free_quantity)
-        charge_quantity = (quotient * charge_for_quantity) + remainder
+        bundles, remainder = divmod(cart_item.quantity, self.charge_for_quantity + self.free_quantity)
+        if remainder > self.charge_for_quantity:
+            bundles += 1
+            remainder = 0
+        charge_quantity = (bundles * self.charge_for_quantity) + remainder
         return store.get_product_price(cart_item.product) * charge_quantity
+
+
+class DependentDiscountOffer(AbstractOffer):
+    '''A percentage discount is applied to the target_product in the presence
+    of another product.'''
+
+    def __init__(self, *args, **kwargs):
+        super(DependentDiscountOffer, self).__init__(*args, **kwargs)
+
+    def calculate_line_total(self, cart_item, store):
+        pass
